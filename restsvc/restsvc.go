b72e2c681b387a689c9c25f35cf5548c379050c5
d-smith <doug.smith.mail@gmail.com>
2015-02-02 16:43:58 -0700

    Refactor 1 - default not handled implementation

--- restsvc/restsvc.go
+++ restsvc/restsvc.go
@@ -1,6 +1,14 @@
 /*
-Worked through the sleepy example from 
+Worked through the REST example from 
 http://dougblack.io/words/a-restful-micro-framework-in-go.html
+
+1st change - need a way to default a method not supported response instead
+of declaring not supported methods for each HTTP method that each resource type
+must include. Ok, I peeked at sleepy... seriously go here for the good stuff:
+
+https://github.com/dougblack/sleepy
+
+This code is a learning exercise.
 */
 
 
@@ -18,77 +26,78 @@ func response(rw http.ResponseWriter, request *http.Request) {
 	rw.Write([]byte("Hello world."))
 }
 
-type Resource interface {
-	Get(values url.Values)(int, interface{})
-	Post(values url.Values)(int, interface{})
+type GetSupported interface {
+	Get(url.Values, http.Header) (int, interface{}, http.Header)
+}
+
+type PostSupported interface {
+	Post(url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 
-type (
-	GetNotSupported struct{}
-	PostNotSupported   struct{}
-)
-
-func (GetNotSupported) Get(values url.Values) (int, interface{}) {
-    return 405, ""
-}
-
-func (PostNotSupported) Post(values url.Values) (int, interface{}) {
-    return 405, ""
-}
 
 //Receiver for methods that manage our resources
 type API struct {} 
 
-func (api *API) Abort(rw http.ResponseWriter, statusCode int) {
-    rw.WriteHeader(statusCode)
-}
 
-func (api *API) requestHandler(resource Resource) http.HandlerFunc {
-	fmt.Println("request handler called")
+//TODO - can I use a type switch here? (see Effective Go)
+func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 	return func(rw http.ResponseWriter, request *http.Request) {
 		
-		var data interface{}
-        var code int
         
-		method := request.Method
-		request.ParseForm()
-		values := request.Form
+		request.ParseForm()  //TODO: error handling
 		
-		switch method {
+		var handler func(url.Values, http.Header) (int, interface{}, http.Header)
+		
+		switch request.Method {
 			case "GET":
-				code, data = resource.Get(values)
+				if resource, ok := resource.(GetSupported); ok {
+					handler = resource.Get	
+				}
+			case "POST":
+				if resource, ok := resource.(PostSupported); ok {
+					handler = resource.Post	
+				} 
 		}
+		
+		
+		if handler == nil {
+			rw.WriteHeader(500)
+			return
+		}
+		
+		code, data, header := handler(request.Form, request.Header)
 		
 		content, err := json.Marshal(data)
 		if err != nil {
-			api.Abort(rw, 500)
+			rw.WriteHeader(500)
+			return
 		}
 		
+		for name, values := range header {
+			for _, value := range values {
+				rw.Header().Add(name, value)
+			}
+		}
 		rw.WriteHeader(code)
 		rw.Write(content)
 		
 	}
 }
 
-func (api *API) AddResource(resource Resource, path string) {
+func (api *API) AddResource(resource interface{}, path string) {
     http.HandleFunc(path, api.requestHandler(resource))
-    fmt.Println("handle added")
 }
 
-type HelloResource struct {
-	PostNotSupported
-}
+type HelloResource struct {}
 
-func (HelloResource) Get(values url.Values) (int, interface{}) {
+func (HelloResource) Get(values url.Values, headers http.Header) (int, interface{}, http.Header) {
     data := map[string]string{"hello": "world"}
-    return 200, data
+    return 200, data, http.Header{"Content-type": {"application/json"}}
 }
 
 func (api *API) Start(port int) {
-	fmt.Println("api Start called")
     portString := fmt.Sprintf(":%d", port)
-    fmt.Printf("listen and serve on %s\n", portString)
     err := http.ListenAndServe(portString, nil)
     if err != nil {
     	log.Fatal("ListenAndServe: ", err)
