7b14499aa2943f3e18297c7f6b9407a5e630a504
d-smith <doug.smith.mail@gmail.com>
2015-02-06 10:31:17 -0700

    Simple consul client example - read kv pairs

---
+++ consul-kv-read/main.go
@@ -0,0 +1,26 @@
+package main 
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/hashicorp/consul/api"
+)
+
+// List some keys prepopulated in consul via the admin console.
+func main() {
+	client, _ := api.NewClient(api.DefaultConfig())
+	kv := client.KV()
+	
+	pairs, _, err := kv.List("/listeners/",nil)
+	if err != nil {
+		log.Panic(err)
+	}
+	
+	fmt.Printf("%v pairs returned\n", len(pairs))
+	
+	for i := range pairs {
+		pair := pairs[i]
+		fmt.Printf("key %s value %s\n", pair.Key, pair.Value)	
+	}
+}
+
