bbdb1674a352563215d17bf9bef59a5332939699
d-smith <doug.smith.mail@gmail.com>
2015-02-09 15:24:46 -0700

    Added example of pluggable component usage

--- hello/hello.go
+++ hello/hello.go
@@ -5,8 +5,51 @@ import (
 	"github.com/d-smith/go-examples/stringutil"
 )
 
+type KVStore interface {
+	Put(string, string)
+	Get(string) string
+}
+
+type A struct {
+	Store KVStore
+}
+
+func NewA(store KVStore) *A {
+	return &A {
+		Store : store,
+	}
+}
+
+func (a *A) doFoo(k string, v string) {
+	a.Store.Put(k,v)
+}
+
+type KV1 struct{}
+func (*KV1) Put(key string, vsl string) {
+	fmt.Println("kv1 put")
+}
+
+func (*KV1) Get(key string) string { 
+	return "kv1 get"
+}
+
+type KV2 struct{}
+func (*KV2) Put(key string, vsl string) {
+	fmt.Println("kv2 put")
+}
+
+func (*KV2) Get(key string) string { 
+	return "kv2 get"
+}
+
+
+
 func main() {
 	greeting := "Hello, world"
 	doubleReveresed := stringutil.Reverse(stringutil.Reverse(greeting))
 	fmt.Printf("%s\n", doubleReveresed)
+	
+	kvs := new(KV1)
+	a := NewA(kvs)
+	a.doFoo("a", "b")
 }
