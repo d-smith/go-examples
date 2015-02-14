7d527eade8fcc289dcd979d8ee696a0e480803c8
d-smith <doug.smith.mail@gmail.com>
2015-02-14 06:41:19 -0700

    Added base64 sample

---
+++ base64/base64.go
@@ -0,0 +1,19 @@
+package main 
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	s := "This is a test of base64 encoding, which is pretty easy"
+	encoded := base64.StdEncoding.EncodeToString([]byte(s))
+	fmt.Println("Encoded: ", encoded)
+	decoded,err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+	} else {
+		fmt.Print("Decoded: ", string(decoded))
+	}
+}
+
