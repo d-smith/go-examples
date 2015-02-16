a88ba72f005d1a01bbd0345b217c11c6a3c8124b
d-smith <doug.smith.mail@gmail.com>
2015-02-16 15:39:39 -0700

    Second template example

--- template/template.go
+++ template/template.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func main() {
+func map1() {
 	sampleTemplate := `{ "{{.foo}}" : "{{.bar}}" }`
 
 	tmpl, err := template.New("test").Parse(sampleTemplate)
@@ -20,4 +20,28 @@ func main() {
 	data["bar"] = "Bar"
 
 	tmpl.Execute(os.Stdout, data)
+}
+
+func map2() { 
+	//TODO - get the commas right via use of array and index=0 test
+	sampleTemplate := 
+	`
+{ {{range $key,$value := . }}{ "{{$key}}" : "{{$value}}" },{{end}} }
+	`
+	tmpl, err := template.New("test").Parse(sampleTemplate)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	
+	data := make(map[string]string)
+	data["foo"] = "Foo"
+	data["bar"] = "Bar"
+	
+	tmpl.Execute(os.Stdout, data)
+ }
+
+func main() {
+	map1()
+	map2()
 }
