979709317afc7d5f6d58ca7ac80adcdfaefe471c
d-smith <doug.smith.mail@gmail.com>
2015-02-18 07:10:01 -0700

    Added to stream xml sample

--- xml/streaming.go
+++ xml/streaming.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"encoding/xml"
-	"strings"
 	"fmt"
+	"strings"
 )
 
 func streaming() {
-	
+
 	xmlLiteral := `
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:schemas-xtrac-fmr-com:b2b">
    <soapenv:Header>
@@ -20,23 +20,42 @@ func streaming() {
          <urn:ControlNo>123</urn:ControlNo>
          <urn:Memo>some notes</urn:Memo>
          <urn:Note>c e-flat f-sharp a</urn:Note>
+         <urn:Foo a='1' b='2'/>
       </urn:AddNote>
    </soapenv:Body>
 </soapenv:Envelope>
 	`
-	
+
 	reader := strings.NewReader(xmlLiteral)
 	decoder := xml.NewDecoder(reader)
-	
+
 	for {
-		t,_ := decoder.Token()
+		t, _ := decoder.Token()
 		if t == nil {
 			break
 		}
-		
-		fmt.Println(t)
+
+		switch t.(type) {
+		default:
+			fmt.Println("Not sure what we're dealing with here")
+		case xml.StartElement:
+			fmt.Println("StartElement")
+			fmt.Printf("\tElement: %s\n", t.(xml.StartElement).Name)
+			attrs := t.(xml.StartElement).Attr
+			for _,attr := range attrs {
+				fmt.Printf("\t attr: %s\n", attr)
+			}
+		case xml.EndElement:
+			fmt.Println("EndElement")
+		case xml.CharData:
+			fmt.Println("CharData")
+		case xml.Comment:
+			fmt.Println("Comment")
+		case xml.ProcInst:
+			fmt.Println("ProcInst")
+		case xml.Directive:
+			fmt.Println("Directive")
+		}
 	}
-	
+
 }
-
-
