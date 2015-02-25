b331530adc488f0bffe0890cf69455e7a0d47cde
d-smith <doug.smith.mail@gmail.com>
2015-02-25 06:27:05 -0700

    Updated sample to act more repl like

--- cli/mycli.go
+++ cli/mycli.go
@@ -1,45 +1,65 @@
 package main
 
 import (
-    "log"
-    "os"
-    "github.com/mitchellh/cli"
-    commands "github.com/d-smith/go-examples/cli/commands"
+	commands "github.com/d-smith/go-examples/cli/commands"
+	"github.com/mitchellh/cli"
+	"os"
+	"strings"
 )
 
+//Can't reuse CLI in a repl because only the first command is run via
+// sync.Once
+func makeCLI() *cli.CLI {
+	c := cli.NewCLI("commands", "1.0.0")
 
+	//c.Args = os.Args[1:]
 
+	fooCommand := &commands.FooCommand{
+		HelpText:     "foo blah blah blah flibby dibby do",
+		SynopsisText: "do some foo",
+	}
 
+	barCommand := &commands.BarCommand{
+		HelpText:     "bar bar barness",
+		SynopsisText: "when you want bar",
+	}
 
-func main() {
-    c := cli.NewCLI("mycli", "1.0.0")
-    c.Args = os.Args[1:]
-    
-    fooCommand := &commands.FooCommand{
-				HelpText: "foo blah blah blah flibby dibby do",
-				SynopsisText: "do some foo",
-			}
-    
-    barCommand := &commands.BarCommand {
-    	HelpText: "bar bar barness",
-    	SynopsisText: "when you want bar",
-    }
-    
-    
-    c.Commands = map[string]cli.CommandFactory{
-        "foo": func() (cli.Command, error) {
+	c.Commands = map[string]cli.CommandFactory{
+		"foo": func() (cli.Command, error) {
 			return fooCommand, nil
 		},
-        
-        "bar": func() (cli.Command, error) {
-        	return barCommand, nil
-        },
-    }
 
-    exitStatus, err := c.Run()
-    if err != nil {
-        log.Println(err)
-    }
+		"bar": func() (cli.Command, error) {
+			return barCommand, nil
+		},
+	}
 
-    os.Exit(exitStatus)
+	return c
+}
+
+func main() {
+	ui := &cli.BasicUi{Reader: os.Stdin, Writer: os.Stdout}
+
+	//c.Args = os.Args[1:]
+
+	for {
+		//TODO - override default 'usage' output when nothing given as a command
+		c := makeCLI()
+		args, err := ui.Ask("What should I do?")
+		if err != nil {
+			if err.Error() == "EOF" || err.Error() == "interrupted" {
+				os.Exit(0)
+			}
+			ui.Error("Error reading input: " + err.Error())
+		} else {
+			c.Args = strings.Fields(args)
+			_, err := c.Run()
+			if err != nil {
+				ui.Error("Error executing command" + err.Error())
+			}
+
+		}
+
+	}
+
 }
