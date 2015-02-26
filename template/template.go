796af8d57465bf071157e3ae7edbd66ef9293508
d-smith <doug.smith.mail@gmail.com>
2015-02-26 07:33:34 -0700

    Fixed comma placement

--- template/template.go
+++ template/template.go
@@ -26,7 +26,8 @@ func map2() {
 	//TODO - get the commas right via use of array and index=0 test
 	sampleTemplate :=
 		`
-{ {{range $key,$value := . }}{ "{{$key}}" : "{{$value}}" },{{end}} }
+{ {{range $key,$value := . }}
+{ "{{$key}}" : "{{$value}}" },{{end}} }
 	`
 	tmpl, err := template.New("test").Parse(sampleTemplate)
 	if err != nil {
@@ -41,10 +42,34 @@ func map2() {
 	tmpl.Execute(os.Stdout, data)
 }
 
+type NameVal struct {
+	Name string
+	Val  string
+}
+
+func mapToKVPair(data map[string]string) []*NameVal {
+	pairs := make([]*NameVal, 0)
+	for k, v := range data {
+		pair := new(NameVal)
+		pair.Name = k
+		pair.Val = v
+		pairs = append(pairs, pair)
+	}
+
+	return pairs
+}
+
+//TODO - output to buffer, fix whitespace in buffer
 func map3() {
 	sampleTemplate :=
 		`
-{ {{range $key,$value := .foo }}{ "{{$key}}" : "{{$value}}" },{{end}} }
+[ {{range $index,$elem := . }}
+	{{if $index}}
+		,{ "{{$elem.Name}}" : "{{$elem.Val}}" }
+	{{else}}
+		{ "{{$elem.Name}}" : "{{$elem.Val}}" }
+	{{end}}
+{{end}}]
 	`
 
 	tmpl, err := template.New("test").Parse(sampleTemplate)
@@ -53,17 +78,17 @@ func map3() {
 		return
 	}
 
-	data := make(map[string]interface{})
 	kvpairs := make(map[string]string)
 	kvpairs["foo"] = "Foo"
 	kvpairs["bar"] = "Bar"
-	data["foo"] = kvpairs
-	
+
+	data := mapToKVPair(kvpairs)
+
 	tmpl.Execute(os.Stdout, data)
 }
 
 func main() {
-	map1()
-	map2()
+	//map1()
+	//map2()
 	map3()
 }
