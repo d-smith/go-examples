17704224a57b6d615beb80dc165fd7ef39500573
d-smith <doug.smith.mail@gmail.com>
2015-03-10 07:03:27 -0600

    Lint clean up

--- stack/stack.go
+++ stack/stack.go
@@ -1,32 +1,41 @@
 package stack
 
+//Stack represents a stack data structure. Elements can be pushed on to and 
+//popped off of the stack
 type Stack struct {
 	size int
-	top  *StackElement
+	top  *Element
 }
 
-type StackElement struct {
+//Element holds an element on the stack
+type Element struct {
 	value interface{}
-	next  *StackElement
+	next  *Element
 }
 
+//Size returns the number of elements on the stack
 func (s *Stack) Size() int {
 	return s.size
 }
 
+//Push pushes an element on the stack
 func (s *Stack) Push(value interface{}) {
-	s.top = &StackElement{value: value, next: s.top}
+	s.top = &Element{value: value, next: s.top}
 	s.size++
 }
 
+//Peek returns the data associated with the top element in the stack, without
+//popping the element off the stack
 func (s *Stack) Peek() interface{} {
 	if s.size > 0 {
 		return s.top.value
-	} else {
-		return nil
 	}
+	
+	return nil
 }
 
+//Pop removes the top element from the stack, returning the data
+//associated with it.
 func (s *Stack) Pop() (value interface{}) {
 	if s.size == 0 {
 		return nil
