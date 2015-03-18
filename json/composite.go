da049c94dc4ef7503a16052e2ac934af4ceeee15
d-smith <doug.smith.mail@gmail.com>
2015-03-18 07:38:13 -0600

    Added example of using same struct for json and xml marshalling

--- json/composite.go
+++ json/composite.go
@@ -2,31 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 )
 
-type managedService struct {
-	Address      string
-	ListenerName string
-	Routes       []route
+type managedService struct { 
+	XMLName struct{}  `xml:"xmlManagedService"`
+	Address      string `json:"address" xml:"xmlAddress"`
+	ListenerName string `json:"listenerName" xml:"xmlListenerName"`
+	Routes       []route `json:"routes" xml:"xmlRoutes"`
 }
 
 type route struct {
-	Name    string
-	URIRoot string
-	Backend backend
+	Name    string `json:"name" xml:"xmlName"`
+	URIRoot string `json:"uriRoot" xml:"xmlUriRoot"`
+	Backend backend `json:"backend" xml:"xmlBackend"`
 }
 
 type backend struct {
-	Name    string
-	Servers []server
+	Name    string `json:"name" xml:"xmlName"`
+	Servers []server `json:"servers" xml:"xmlServers"`
 }
 
 type server struct {
-	Name    string
-	Address string
-	PingURI string
-	Up      bool
+	Name    string `json:"name" xml:"xmlName"`
+	Address string `json:"address" xml:"xmlAddress"`
+	PingURI string `json:"pingUri" xml:"xmlPingUri"`
+	Up      bool `json:"up" xml:"xmlUp"`
 }
 
 func compositeMain() {
@@ -59,8 +61,26 @@ func compositeMain() {
 		}]
 	}
 	`
-	ms := new(managedService)
-	json.Unmarshal([]byte(jsonDoc),ms)
+	fmt.Printf("native json: %s\n",  jsonDoc)
 	
-	fmt.Println(ms)
+	ms := new(managedService)
+	fmt.Printf("parsed document: %s\n", ms)
+	
+	
+	json.Unmarshal([]byte(jsonDoc),ms)
+	xmlBytes, err := xml.Marshal(ms)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	
+	fmt.Printf("marshalled xml: %s\n", string(xmlBytes))
+	
+	jsonBytes, err := json.Marshal(ms)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	
+	fmt.Printf("marshalled json: %s\n", string(jsonBytes))
+	
+	
 }
