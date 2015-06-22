06c35cf80890e905b79dc48a3dc5365097b8d105
d-smith <doug.smith.mail@gmail.com>
2015-06-22 08:24:23 -0600

    More host info

--- network-addr/main.go
+++ network-addr/main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
-func main() {
+func printHostName() {
+	host, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Hostname: ", host)
+}
+
+func printInterfaceAddresses() {
+
 	addresses, err := net.InterfaceAddrs()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
+	fmt.Printf("Interface addresses:")
 	for _, address := range addresses {
-		fmt.Printf("network form: %s, string form: %s\n", address.Network(), address.String())
+		fmt.Printf("\tnetwork form: %s, string form: %s\n", address.Network(), address.String())
+	}
+}
+
+func printInterfaces() {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
 
+	fmt.Printf("Interfaces:")
+	for _, i := range interfaces {
+		fmt.Printf("\t%v\n", i)
+	}
+}
+
+func main() {
+	printHostName()
+	printInterfaceAddresses()
+	printInterfaces()
 }
