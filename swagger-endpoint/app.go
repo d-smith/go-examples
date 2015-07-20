b91c94a7076d6f432d2c885d8643abc60f5ba173
d-smith <doug.smith.mail@gmail.com>
2015-07-20 10:11:57 -0600

    Modified swagger example to invoke the stock quote service

--- swagger-endpoint/app.go
+++ swagger-endpoint/app.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gopkg.in/xmlpath.v1"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 )
 
-var responseJson string = `
+var stubbedResponseJson string = `
 {
   "name": "GOOGL",
   "last": 1002.20,
@@ -15,9 +22,147 @@ var responseJson string = `
 }
 `
 
+type Response struct {
+	Name string  `json:name"Name"`
+	Last float64 `json:name"Last"`
+	Time string  `json:name"Time"`
+	Date string  `json:name"Date"`
+}
+
+var soapStart string = `
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.webserviceX.NET/">
+<soapenv:Header/><soapenv:Body><web:GetQuote><web:symbol>`
+
+var soapEnd string = `</web:symbol></web:GetQuote></soapenv:Body></soapenv:Envelope>`
+
+func getQuote(r *http.Request) (string, error) {
+
+	//Get the symbol as the last part of the request URI
+	parts := strings.Split(r.RequestURI, "/")
+	symbol := parts[len(parts)-1]
+	fmt.Printf("requesting quote for %s\n", symbol)
+
+	payload := fmt.Sprintf("%s%s%s", soapStart, symbol, soapEnd)
+
+	client := &http.Client{}
+	quoteReq, err := http.NewRequest("POST", "http://www.webservicex.net/stockquote.asmx", strings.NewReader(payload))
+	if err != nil {
+		return "", err
+	}
+
+	quoteReq.Header.Add("Content-Type", "text/xml")
+
+	resp, err := client.Do(quoteReq)
+	if err != nil {
+		return "", err
+	}
+
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil
+	}
+	resp.Body.Close()
+	return string(respData), nil
+
+}
+
+func getQuoteResponse(soapResponse string) (string, error) {
+	compiledPath := xmlpath.MustCompile("/Envelope/Body/GetQuoteResponse/GetQuoteResult")
+	root, err := xmlpath.Parse(strings.NewReader(soapResponse))
+	if err != nil {
+		return "", err
+	}
+
+	value, ok := compiledPath.String(root)
+	if !ok {
+		return "", errors.New("Unable to extract GetQuoteResult")
+	}
+
+	quoteResult := string(value)
+
+	return quoteResult, nil
+}
+
+func getQuotePart(quoteResult string, partName string) (string, error) {
+	compiledPath := xmlpath.MustCompile("/StockQuotes/Stock/" + partName)
+	root, err := xmlpath.Parse(strings.NewReader(quoteResult))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value, ok := compiledPath.String(root)
+	if !ok {
+		return "", errors.New("Unable to extract " + partName)
+	}
+
+	return string(value), nil
+}
+
+func formResponseJson(data string) ([]byte, error) {
+
+	//Pull out the quote result part of the soap envelope
+	quoteResult, err := getQuoteResponse(data)
+	if err != nil {
+		return nil, err
+	}
+
+	//From the quote result, pull out the name
+	name, err := getQuotePart(quoteResult, "Name")
+	if err != nil {
+		return nil, nil
+	}
+
+	//Grab the last value
+	last, err := getQuotePart(quoteResult, "Last")
+	if err != nil {
+		return nil, nil
+	}
+
+	lastFloat, err := strconv.ParseFloat(last, 64)
+	if err != nil {
+		return nil, nil
+	}
+
+	//Time
+	time, err := getQuotePart(quoteResult, "Time")
+	if err != nil {
+		return nil, nil
+	}
+
+	//Date
+	date, err := getQuotePart(quoteResult, "Date")
+	if err != nil {
+		return nil, nil
+	}
+
+	response := Response{
+		Name: name,
+		Last: lastFloat,
+		Time: time,
+		Date: date,
+	}
+
+	return json.Marshal(response)
+}
+
 func handleQuoteCalls(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
+
+	quoteData, err := getQuote(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	responseJson, err := formResponseJson(quoteData)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
 	w.Write([]byte(responseJson))
 }
 
