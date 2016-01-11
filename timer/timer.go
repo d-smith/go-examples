a923f50cfc82ed399e665e91600255175ee8dbd0
d-smith <doug.smith.mail@gmail.com>
2016-01-11 15:03:59 -0800

    Added contributor timings, error info

--- timer/timer.go
+++ timer/timer.go
@@ -1,4 +1,5 @@
 package timer
+
 import "time"
 
 type BackendCall struct {
@@ -7,22 +8,25 @@ type BackendCall struct {
 }
 
 type Contributor struct {
-	Name string
-	Time time.Duration
+	Name         string
+	Time         time.Duration
+	Error        error
+	start        time.Time
 	BackendCalls []BackendCall
 }
 
 type APITime struct {
-	Name string
-	Time time.Duration
-	Contributors []Contributor
-	Error error
-	start time.Time
+	Name         string
+	Time         time.Duration
+	Contributors []*Contributor
+	ErrorFree    bool
+	Error        error
+	start        time.Time
 }
 
 func NewAPITimer(name string) *APITime {
-	return &APITime {
-		Name: name,
+	return &APITime{
+		Name:  name,
 		start: time.Now(),
 	}
 }
@@ -30,6 +34,31 @@ func NewAPITimer(name string) *APITime {
 func (t *APITime) Stop(err error) {
 	t.Time = time.Now().Sub(t.start)
 	t.Error = err
+	t.ErrorFree = len(t.ContributorErrors()) == 0 && t.Error == nil
 }
 
+func (t *APITime) StartContributor(name string) *Contributor {
+	contributor := &Contributor{
+		Name:  name,
+		start: time.Now(),
+	}
 
+	t.Contributors = append(t.Contributors, contributor)
+
+	return contributor
+}
+
+func (t *APITime) ContributorErrors() []error {
+	var errs []error
+	for _, c := range t.Contributors {
+		if c.Error != nil {
+			errs = append(errs, c.Error)
+		}
+	}
+	return errs
+}
+
+func (c *Contributor) End(err error) {
+	c.Time = time.Now().Sub(c.start)
+	c.Error = err
+}
--- timer/timer_test.go
+++ timer/timer_test.go
@@ -1,6 +1,7 @@
 package timer
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -8,6 +9,49 @@ func TestPostitiveDuration(t *testing.T) {
 	at := NewAPITimer("foo")
 	at.Stop(nil)
 	if at.Time == 0 {
+		t.Fail()
+	}
+
+}
+
+func TestContributors(t *testing.T) {
+	at := NewAPITimer("foo")
+	c1 := at.StartContributor("c1")
+	c2 := at.StartContributor("c2")
+	c2.End(nil)
+	c1.End(nil)
+	at.Stop(nil)
+
+	if at.Error != nil {
+		t.Fail()
+	}
+
+	if c1.Time <= 0 || c2.Time <= 0 {
+		t.Fail()
+	}
+
+	if at.ErrorFree == false {
+		t.Fail()
+	}
+}
+
+func TestIfContributorErrorsThenTimerErrors(t *testing.T) {
+	at := NewAPITimer("foo")
+	c1 := at.StartContributor("c1")
+	c2 := at.StartContributor("c2")
+	c2.End(errors.New("oh whoops"))
+	c1.End(nil)
+	at.Stop(nil)
+
+	if at.Error != nil {
+		t.Fail()
+	}
+
+	if len(at.ContributorErrors()) != 1 {
+		t.Fail()
+	}
+
+	if at.ErrorFree == true {
 		t.Fail()
 	}
 }
