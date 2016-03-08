19458dbb157e130ab9ba519320fe079dba4e84cb
d-smith <doug.smith@fmr.com>
2016-03-08 14:29:18 -0800

    Added gather of mac addresses and hash of them

---
+++ macdaddy/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net"
+	"log"
+	"golang.org/x/crypto/scrypt"
+	"io"
+	"crypto/rand"
+	"fmt"
+	"crypto/sha256"
+)
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+const (
+	SaltBytes = 32
+	UidBytes = 32
+)
+
+func nonSaltyHash(mac string) string {
+	hash := sha256.Sum256([]byte(mac))
+	return fmt.Sprintf("%x",hash)
+}
+
+func saltyHash(mac string) string {
+	salt := make([]byte, SaltBytes)
+	_, err := io.ReadFull(rand.Reader, salt)
+	fatal(err)
+
+	hash, err := scrypt.Key([]byte(mac), salt, 16384, 8, 1, UidBytes)
+	fatal(err)
+
+	return fmt.Sprintf("%x",hash)
+}
+
+
+func main() {
+	ifaces, err := net.Interfaces()
+	fatal(err)
+
+	for _, iface := range ifaces {
+		log.Printf("iface %s has mac address %s and its salty has is %s\n",
+			iface.Name, iface.HardwareAddr, nonSaltyHash(iface.HardwareAddr.String()))
+	}
+}
