ecae206574fe087bd3f677a4f313a7a2fc3bd570
d-smith <doug.smith@fmr.com>
2016-05-04 08:30:26 -0400

    Implemented create from event history

--- es2/internal/features/aggregates/agghistory_steps.go
+++ es2/internal/features/aggregates/agghistory_steps.go
@@ -1,22 +1,49 @@
 package aggregates
 
-import . "github.com/lsegal/gucumber"
+import (
+	"github.com/d-smith/go-examples/es2"
+	"github.com/d-smith/go-examples/es2/sample"
+	. "github.com/lsegal/gucumber"
+	"github.com/stretchr/testify/assert"
+)
 
 func init() {
+	var eventHistory []es2.Event
+	var userFromHistory *sample.User
+
 	Given(`^an event sourced aggregate's event history$`, func() {
+		eventHistory = []es2.Event{
+			es2.Event{
+				Payload: sample.UserCreated{
+					AggregateId: "123",
+					FirstName:   "orig first",
+					LastName:    "orig last",
+					Email:       "orig@email.com",
+				},
+			},
+			es2.Event{
+				Payload: sample.UserFirstNameUpdated{
+					NewFirst: "new first",
+				},
+			},
+		}
 
 	})
 
 	When(`^I instantiate the aggregate from its history$`, func() {
-
+		userFromHistory = sample.NewUserFromHistory(eventHistory)
+		assert.NotNil(T, userFromHistory, "Nil user build from history")
 	})
 
 	Then(`^the instance state is correct$`, func() {
-
+		assert.Equal(T, "123", userFromHistory.ID)
+		assert.Equal(T, "new first", userFromHistory.FirstName)
+		assert.Equal(T, "orig last", userFromHistory.LastName)
+		assert.Equal(T, "orig@email.com", userFromHistory.Email)
 	})
 
 	And(`^there are no uncommitted events$`, func() {
-		T.Skip() // pending
+		assert.Equal(T, 0, len(userFromHistory.Events))
 	})
 
 }
--- es2/sample/user.go
+++ es2/sample/user.go
@@ -35,11 +35,33 @@ func NewUser(first, last, email string) (*User, error) {
 	return user, nil
 }
 
+func NewUserFromHistory(events []es2.Event) *User {
+	user := new(User)
+	user.Aggregate = es2.NewAggregate()
+
+	for _, e := range events {
+		log.Println("apply event", e)
+		user.Route(e)
+	}
+
+	return user
+}
+
 type UserCreated struct {
 	AggregateId string
 	FirstName   string
 	LastName    string
 	Email       string
+}
+
+type UserFirstNameUpdated struct {
+	OldFirst string
+	NewFirst string
+}
+
+type UserLastNameUpdated struct {
+	OldLast string
+	NewLast string
 }
 
 func (u *User) handleUserCreated(event UserCreated) {
@@ -49,10 +71,16 @@ func (u *User) handleUserCreated(event UserCreated) {
 	u.Email = event.Email
 }
 
+func (u *User) handleUserFirstNameUpdate(event UserFirstNameUpdated) {
+	u.FirstName = event.NewFirst
+}
+
 func (u *User) Route(event es2.Event) {
 	switch event.Payload.(type) {
 	case UserCreated:
 		u.handleUserCreated(event.Payload.(UserCreated))
+	case UserFirstNameUpdated:
+		u.handleUserFirstNameUpdate(event.Payload.(UserFirstNameUpdated))
 	default:
 		log.Println("WARN: unknown event routed to User aggregate")
 	}
