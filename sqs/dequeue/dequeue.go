72213037bc506f346dd811fc20bb35fa8cd8f4b7
d-smith <doug.smith@fmr.com>
2016-05-10 09:24:34 +0100

    Added sqs dequeue

---
+++ sqs/dequeue/dequeue.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage: dequeue <queue url>")
+		os.Exit(1)
+	}
+
+	svc := sqs.New(session.New(), &aws.Config{Region: aws.String("us-east-1")})
+
+	params := &sqs.ReceiveMessageInput{
+		QueueUrl: aws.String(os.Args[1]),
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout: aws.Int64(1),
+		WaitTimeSeconds:   aws.Int64(1),
+	}
+	resp, err := svc.ReceiveMessage(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(resp.Messages)
+	if len(resp.Messages) == 0 {
+		return
+	}
+
+	delParams := &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(os.Args[1]),
+		ReceiptHandle: resp.Messages[0].ReceiptHandle,
+	}
+	delResp, err := svc.DeleteMessage(delParams)
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(delResp)
+}
