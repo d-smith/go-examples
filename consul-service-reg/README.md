a36be910c8c069aa1e8cbb9f0387781b1741ca58
d-smith <doug.smith@fmr.com>
2016-05-24 08:29:22 -0700

    Added consul service lookup sample

---
+++ consul-service-reg/README.md
@@ -0,0 +1,14 @@
+## Synopsis
+
+Read a service definition using the consul API
+
+### Set Up
+
+See README.md in ../consul-template
+
+* Run consul
+* Load the env1 service definition (use json and instructions in ../consult-template)
+
+### Dependencies
+
+go get github.com/hashicorp/consul/api
---
+++ consul-service-reg/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/hashicorp/consul/api"
+	"log"
+)
+func main() {
+	config := api.DefaultConfig()
+	config.Address = "localhost:8500"
+	client,err := api.NewClient(config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	catalog := client.Catalog()
+	env1Services,_,err := catalog.Service("demo-service","env1",nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	env1Service := env1Services[0]
+
+	log.Printf("Call env1 demo-service at %s:%d\n",env1Service.ServiceAddress, env1Service.ServicePort)
+
+	log.Println("bye for now")
+}
