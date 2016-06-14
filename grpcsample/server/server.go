3b93eebaec6be43ae95e690d9220145be4949162
d-smith <doug.smith@fmr.com>
2016-06-14 10:35:02 -0700

    Added chained middleware to the sample

--- grpcsample/server/server.go
+++ grpcsample/server/server.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	. "github.com/d-smith/go-examples/grpcsample"
+	"github.com/mwitkow/go-grpc-middleware"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
@@ -11,6 +12,16 @@ import (
 )
 
 type MyDirectoryServer struct{}
+
+func LogRequest(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	grpclog.Printf("Request:%v", req)
+	return handler(ctx, req)
+}
+
+func LogDestination(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	grpclog.Printf("Dest:%v", info.FullMethod)
+	return handler(ctx, req)
+}
 
 func (mds *MyDirectoryServer) LookupPersonByName(context.Context, *NameRequest) (*Person, error) {
 	return &Person{
@@ -35,10 +46,11 @@ func main() {
 		grpclog.Fatalf("failed to listen: %v", err)
 	}
 
-	var opts []grpc.ServerOption
+	//var opts []grpc.ServerOption
 	var grpcServer *grpc.Server
 
-	grpcServer = grpc.NewServer(opts...)
+	grpcServer = grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(LogRequest, LogDestination)))
 	RegisterDirectoryServer(grpcServer, newServer())
 	grpcServer.Serve(lis)
 }
