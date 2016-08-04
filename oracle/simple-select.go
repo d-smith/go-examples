28b95592b812b6c972cf3b3afd6f66d7d3ff4baf
d-smith <doug.smith.mail@gmail.com>
2016-08-04 08:37:04 -0700

    Added timestamp sample to oracle sample

--- oracle/simple-select.go
+++ oracle/simple-select.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-oci8"
+	"time"
 )
 
 func main() {
@@ -15,6 +16,11 @@ func main() {
 	defer db.Close()
 
 	if err = testSelect(db); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err = testDate(db); err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -36,6 +42,26 @@ func testSelect(db *sql.DB) error {
 		var three int
 		rows.Scan(&foo, &three)
 		fmt.Println(foo, three)
+	}
+
+	return nil
+}
+
+func testDate(db *sql.DB) error {
+	query := "select systimestamp from dual"
+	rows, err := db.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	fmt.Println("rows", rows)
+	fmt.Println(rows.Columns())
+
+	for rows.Next() {
+		var ts time.Time
+		rows.Scan(&ts)
+		fmt.Println(ts)
 	}
 
 	return nil
