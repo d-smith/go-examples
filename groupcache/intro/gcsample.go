d5b3763bc46b8d48b43ba637a4b0187520c7876c
d-smith <doug.smith.mail@gmail.com>
2016-08-10 15:00:48 -0700

    formatting

--- groupcache/intro/gcsample.go
+++ groupcache/intro/gcsample.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "log"
-    "github.com/golang/groupcache"
+	"github.com/golang/groupcache"
+	"log"
 )
 
 func fatal(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-    me := "http://localhost:9000"
-    groupcache.NewHTTPPool(me)
+	me := "http://localhost:9000"
+	groupcache.NewHTTPPool(me)
 
-    var thingGroup = groupcache.NewGroup("thing-group", 64 << 20, groupcache.GetterFunc(
-        func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
-            log.Println("thing group getter called with key", key)
-            dest.SetString(key + " - value")
-            return nil
-        },
-    ))
+	var thingGroup = groupcache.NewGroup("thing-group", 64<<20, groupcache.GetterFunc(
+		func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
+			log.Println("thing group getter called with key", key)
+			dest.SetString(key + " - value")
+			return nil
+		},
+	))
 
-    var sval string
-    log.Println("get foo")
-    err := thingGroup.Get(nil, "foo", groupcache.StringSink(&sval))
-    fatal(err)
+	var sval string
+	log.Println("get foo")
+	err := thingGroup.Get(nil, "foo", groupcache.StringSink(&sval))
+	fatal(err)
 
-    log.Println("read foo val",sval)
+	log.Println("read foo val", sval)
 
-    log.Println("get foo")
-    err = thingGroup.Get(nil, "foo", groupcache.StringSink(&sval))
-    fatal(err)
+	log.Println("get foo")
+	err = thingGroup.Get(nil, "foo", groupcache.StringSink(&sval))
+	fatal(err)
 
-    log.Println("read foo val",sval)
+	log.Println("read foo val", sval)
 
-    log.Println("get bar")
-    err = thingGroup.Get(nil, "bar", groupcache.StringSink(&sval))
-    fatal(err)
+	log.Println("get bar")
+	err = thingGroup.Get(nil, "bar", groupcache.StringSink(&sval))
+	fatal(err)
 
-    log.Println("read bar val",sval)
+	log.Println("read bar val", sval)
 }
