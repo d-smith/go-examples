651ef2d1fdd82f7fd05f94a80b71d32f37af239e
d-smith <doug.smith.mail@gmail.com>
2016-09-08 09:36:55 -0700

    Added backoff/retry for connection establishment

--- oracle/simple-select.go
+++ oracle/simple-select.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	_ "github.com/mattn/go-oci8"
 	"time"
+	"log"
 )
 
 func main() {
+
+	log.Println("Open the database")
 	db, err := sql.Open("oci8", "system/oracle@//localhost:1521/xe.oracle.docker")
 	if err != nil {
 		fmt.Println(err)
@@ -15,14 +18,40 @@ func main() {
 	}
 	defer db.Close()
 
+	log.Println("Ping the db as open might not actually connect")
+
+	//Use a backoff/retry strategy - we can start this client before
+	//the database is started, and see it eventually connect and process
+	//queries
+	var dbError error
+	maxAttempts := 20
+	for attempts := 1; attempts <= maxAttempts; attempts++ {
+		log.Println("ping database...")
+		dbError = db.Ping()
+		if dbError == nil {
+			break
+		}
+		log.Println("Ping failed: ", dbError, "retry in ", attempts, " seconds.")
+		time.Sleep(time.Duration(attempts) * time.Second)
+	}
+	if dbError != nil {
+		log.Fatal(dbError)
+	}
+
+
+	log.Println("Do select")
 	if err = testSelect(db); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	if err = testDate(db); err != nil {
-		fmt.Println(err)
-		return
+	for i:= 0; i < 10000; i++ {
+		if err = testDate(db); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		time.Sleep(5 * time.Second)
 	}
 
 }
@@ -54,9 +83,6 @@ func testDate(db *sql.DB) error {
 		return err
 	}
 	defer rows.Close()
-
-	fmt.Println("rows", rows)
-	fmt.Println(rows.Columns())
 
 	for rows.Next() {
 		var ts time.Time
