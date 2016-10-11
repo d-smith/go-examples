05237994d9aa7a09049e98221fec8d172403663f
d-smith <doug.smith.mail@gmail.com>
2016-10-11 12:17:45 -0700

    Updated oracle sample with read and write of blob

--- oracle/simple-select.go
+++ oracle/simple-select.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	_ "github.com/mattn/go-oci8"
 	"log"
 	"strings"
@@ -10,7 +12,7 @@ import (
 
 func openAndConnectToDb() *sql.DB {
 	log.Println("Open the database")
-	db, err := sql.Open("oci8", "system/oracle@//localhost:1521/xe.oracle.docker")
+	db, err := sql.Open("oci8", "user/password@//localhost:1521/xe.oracle.docker")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,6 +45,18 @@ func connectionErr(err error) bool {
 	return strings.HasPrefix(errStr, "ORA-03114") || strings.HasPrefix(errStr, "ORA-03113")
 }
 
+type ABC struct {
+	A string
+	B int
+	C string
+}
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
 	var db *sql.DB = openAndConnectToDb()
@@ -52,7 +66,7 @@ func main() {
 		log.Println(err)
 	}
 
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 1; i++ {
 		if err := testDate(db); err != nil {
 			log.Println(err)
 
@@ -64,6 +78,14 @@ func main() {
 
 		time.Sleep(5 * time.Second)
 	}
+
+	id := time.Now().Unix()
+	err := insertBlobData(db, id)
+	fatal(err)
+
+	abc, err := readBlobData(db, id)
+	fatal(err)
+	fmt.Printf("%v+v\n", abc)
 
 }
 
@@ -100,4 +122,42 @@ func testDate(db *sql.DB) error {
 	}
 
 	return nil
+}
+
+func insertBlobData(db *sql.DB, id int64) error {
+	abc := ABC{
+		A: "a part", B: 42, C: "you see",
+	}
+
+	abcBytes, err := json.Marshal(&abc)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("insert into blob_sample (id, payload) values (:1,:2)", id, abcBytes)
+	return err
+}
+
+func readBlobData(db *sql.DB, id int64) (ABC, error) {
+	var abc ABC
+
+	rows, err := db.Query("select payload from blob_sample where id = :1", id)
+	if err != nil {
+		return abc, err
+	}
+
+	defer rows.Close()
+
+	var abcBytes []byte
+
+	for rows.Next() {
+		err := rows.Scan(&abcBytes)
+		if err != nil {
+			return abc, err
+		}
+	}
+
+	err = json.Unmarshal(abcBytes, &abc)
+
+	return abc, err
 }
